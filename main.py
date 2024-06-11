a8fcbcfc1ad6c641229a15b501216ef2dce4f61f
Roy Mayan <>
2024-06-11 15:05:25 +0300

    Running custom experiments easily

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ from learner.experiment import *
 # learner config
 LEARNERS = ("Kayne", "Language", "Category", "Word")
 THEORIES = ("Head-initial", "Head-final", "Mixed-category", "Mixed-word")
-LEARNER_CONFIGS = { l : THEORIES for l in LEARNERS}
-
-BLANK_GRAMMAR = KAYNE_GRAMMAR_WITH_EMPTY_DP
+ALL_CONFIGS = { l : THEORIES for l in LEARNERS}
 
 # general
 LOGS_FOLDER = os.path.join("output", "logs")
 
 if __name__ == '__main__':
-    exp = Experiment(LOGS_FOLDER, BLANK_GRAMMAR, "SA")
-    exp.sanity_test(LEARNER_CONFIGS, pp=True, cp=True, coordination=False)
+    # with coordination (including "and")
+    # exp = Experiment(LOGS_FOLDER, KAYNE_GRAMMAR_WITH_EMPTY_DP, "SA")
+    # exp.test_learner("Kayne", "Head-initial", pp=True, cp=True, coordination=True)
+
+    # no coordination
+    exp = Experiment(LOGS_FOLDER, KAYNE_GRAMMAR_WITH_EMPTY_DP_NO_CO, "SA")
+    exp.test_learner("Kayne", "Head-initial", pp=True, cp=True, coordination=False)
+
+    # exp.test_learner("Kayne", "Head-final", pp=True, cp=True, coordination=False)
+    # exp.sanity_test(ALL_CONFIGS, pp=True, cp=True, coordination=False)
